eda9b1e8df7f1dc24d430b655a9a6a30e83d78ad
Dallas Baker <dallas.c.baker@dalbaker.com>
2017-11-16 23:46:44 -0800

    Fizz Buzz Solution

---
+++ app.py
@@ -0,0 +1,27 @@
+def fizz_buzz(number1, number2):
+    for number in range(number1, number2+1):
+        FizzBuzzDict = {
+            "fizz" : multiple(number, 3),
+            "buzz" : multiple(number, 5),
+            "bam" : multiple(number, 12)
+        }
+        myString = make_string(FizzBuzzDict)
+        if(myString != ""):
+            print(myString)
+        else:
+            print("not a multiple")
+
+def multiple(number, multiple):
+    if(number % multiple == 0):
+        return True
+    else:
+        return False
+
+def make_string(dictionary):
+    myString = ""
+    for key, value in dictionary.items():
+        if(value):
+            myString += str(key)
+    return myString
+
+fizz_buzz(1,100)
